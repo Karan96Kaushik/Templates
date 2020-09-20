0896c9cc746083865eced04709708d10da9d732d
Karan@Cathedral <karankaushik69@gmail.com>
2020-09-20 11:19:46 +0530

    tcp

---
+++ tcp_server.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+var server = net.createServer(function (socket) {
+	socket.setEncoding('UTF-8')
+
+    socket.write("Connected")
+    
+	socket.on('close', function (data) {
+		console.log('Connection Closed');
+	});
+
+	socket.on('error', function (data) {
+		console.log('Connection Error');
+    });
+    
+    socket.on('data', function (data) {
+        console.log(data)
+    })
+
+})

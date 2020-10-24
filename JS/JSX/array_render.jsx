5fe7046a3b7eff078a81c274e42528294b5d4438
Karan <karankshk96@gmail.com>
2020-10-24 12:01:25 +0530

    format

--- JS/JSX/array_render.jsx
+++ JS/JSX/array_render.jsx
@@ -1,19 +1,21 @@
 var Projects = [
-    { projectName: "Big1", projectDate: "12:53 / 21/05/16", projectLink:"/projects/1234" },
-    { projectName: "Big2", projectDate: "12:53 / 22/05/16", projectLink:"/projects/1235" },
-    { projectName: "Big3", projectDate: "12:53 / 23/05/16", projectLink:"/projects/1236" },
+    { projectName: "Big1", projectDate: "12:53 / 21/05/16", projectLink: "/projects/1234" },
+    { projectName: "Big2", projectDate: "12:53 / 22/05/16", projectLink: "/projects/1235" },
+    { projectName: "Big3", projectDate: "12:53 / 23/05/16", projectLink: "/projects/1236" },
 ]
 
-(<div className={classes.listContainer}>
+    (
+        <div className={classes.listContainer}>
 
 
-    {Projects.map(project => (
-        <ProjectListItems
-            projectName={project.projectName}
-            projectEditedDate={project.projectDate}
-            projectLink={project.projectLink}
-        />
-    ))}
+            {Projects.map(project => (
+                <ProjectListItems
+                    projectName={project.projectName}
+                    projectEditedDate={project.projectDate}
+                    projectLink={project.projectLink}
+                />
+            ))}
 
 
-</div>)
+        </div>
+    )
--- JS/Sequelize.js
+++ JS/Sequelize.js
@@ -3,33 +3,33 @@ var Sequelize = require("sequelize")
 var sequelize = new Sequelize('postgres://user:pass@irasus.com:5432/postgres');
 
 var User = sequelize.define('user', {
-    firstName: {
-      type: Sequelize.STRING
-    },
-    lastName: {
-      type: Sequelize.STRING
-    }
-  });
-  
-  // force: true will drop the table if it already exists
-  User.sync({force: true}).then(function () {
-    // Table created
-    return User.create({
-      firstName: 'John',
-      lastName: 'Hancock'
-    });
-  });
+	firstName: {
+		type: Sequelize.STRING
+	},
+	lastName: {
+		type: Sequelize.STRING
+	}
+});
+
+// force: true will drop the table if it already exists
+User.sync({ force: true }).then(function () {
+	// Table created
+	return User.create({
+		firstName: 'John',
+		lastName: 'Hancock'
+	});
+});
 
 sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-    User.create({
-        firstName: 'JohnBB',
-        lastName: 'HancockBB'
-      });
+	.authenticate()
+	.then(function (err) {
+		console.log('Connection has been established successfully.');
+		User.create({
+			firstName: 'JohnBB',
+			lastName: 'HancockBB'
+		});
 
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
+	})
+	.catch(function (err) {
+		console.log('Unable to connect to the database:', err);
+	});
